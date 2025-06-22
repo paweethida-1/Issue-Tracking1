637f8f22bc81ad127111cbb5d0133f9e5f2406a4
paweethida-1 <paweethidakham@gmail.com>
2025-06-22 22:07:44 +0700

    notyet

--- backend/routes/request.js
+++ backend/routes/request.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const requestController = require("../controllers/request");
+//const { authCheck } = require('../middlewares/auth');
+
 
 // ผู้ใช้ทั่วไป: สร้างคำร้อง
-router.post('/', requestController.createRequest);
+router.post('/request', requestController.createRequest);
 
 // ผู้ดูแลระบบ: ดูทั้งหมด
-router.get('/', requestController.getAllRequests);
+router.get('/request', requestController.getAllRequests);
 
 // ดูคำร้องรายตัว
-router.get('/:id', requestController.getRequestById);
+router.get('/request/:id', requestController.getRequestById);
 
 // ผู้ดูแลระบบ: เปลี่ยน priority
-router.patch('/:id/priority', requestController.updatePriority);
+router.patch('/request/:id/priority', requestController.updatePriority);
 
 // ผู้ดูแลระบบ: เปลี่ยน status
-router.patch('/:id/status', requestController.updateStatus);
-
+router.patch('/request/:id/status', requestController.updateStatus);
 
 module.exports = router;
